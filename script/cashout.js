c2e27181cac1a7edb8d1ae05b1762bf806a1e595
Mahadi176 <mahadi210176@gmail.com>
2026-02-20 23:58:47 +0600

    some updates

--- script/cashout.js
+++ script/cashout.js
@@ -1,34 +1,32 @@
 document.getElementById('cashout-btn')
 .addEventListener('click', function(){
     // 1. get agent number and validate 
-    const cashoutNumberInput = document.getElementById('cashout-input') ;
-    const cashoutNumber = cashoutNumberInput.value ;
+    const cashoutNumber = getValueFromInput('cashout-number') ;
     if(cashoutNumber.length !='11' ){
         alert('Invalid Agent Number')
         return ;
     }
     // 2.get the amount , validate , covert the number 
-    const cashoutAmountInput = document.getElementById('cashout-amount') ;
-    const cashoutAmount = cashoutAmountInput.value ;
+    const cashoutAmount = getValueFromInput('cashout-amount') ;
+
     // 3. get current balance , validate , covert to number 
-    const balanceElement = document.getElementById('balance');
-    const balance = balanceElement.innerText;
+    const currentBalance = getBalance();
+
     // 4.Calculate New Balance 
-    const newBalance = Number(balance)-Number(cashoutAmount);
-    
+    const newBalance = currentBalance-Number(cashoutAmount);
+
     if(newBalance < 0){
         alert("Invalid Balance")
         return ;
     }
     // 5. get the pin and verify 
-    const cashoutPinInput = document.getElementById('cashout-pin');
-    const cashoutPin = cashoutPinInput.value ;
+    const Pin = document.getValueFromInput('cashout-pin');
     
-    if(cashoutPin == "1234"){
+    if(Pin == "1234"){
         // 6.true :: show alert verify 
         alert("Cashout Successful")
         console.log(newBalance)
-        balanceElement.innerText = newBalance;
+        setBalance(newBalance)
     }
     else{
     // 7. false :: show alert return 
@@ -36,3 +34,43 @@ document.getElementById('cashout-btn')
         return 
         }
 })
+
+
+// document.getElementById('cashout-btn')
+// .addEventListener('click', function(){
+//     // 1. get agent number and validate 
+//     const cashoutNumberInput = document.getElementById('cashout-input') ;
+//     const cashoutNumber = cashoutNumberInput.value ;
+//     if(cashoutNumber.length !='11' ){
+//         alert('Invalid Agent Number')
+//         return ;
+//     }
+//     // 2.get the amount , validate , covert the number 
+//     const cashoutAmountInput = document.getElementById('cashout-amount') ;
+//     const cashoutAmount = cashoutAmountInput.value ;
+//     // 3. get current balance , validate , covert to number 
+//     const balanceElement = document.getElementById('balance');
+//     const balance = balanceElement.innerText;
+//     // 4.Calculate New Balance 
+//     const newBalance = Number(balance)-Number(cashoutAmount);
+    
+//     if(newBalance < 0){
+//         alert("Invalid Balance")
+//         return ;
+//     }
+//     // 5. get the pin and verify 
+//     const cashoutPinInput = document.getElementById('cashout-pin');
+//     const cashoutPin = cashoutPinInput.value ;
+    
+//     if(cashoutPin == "1234"){
+//         // 6.true :: show alert verify 
+//         alert("Cashout Successful")
+//         console.log(newBalance)
+//         balanceElement.innerText = newBalance;
+//     }
+//     else{
+//     // 7. false :: show alert return 
+//         alert("Invalid Pin")
+//         return 
+//         }
+// })
--- script/machine.js
+++ script/machine.js
@@ -1,1 +1,25 @@
 console.log('machine added')
+
+//  machine id -> input value
+function getValueFromInput(id) {
+    const input = document.getElementById(id);
+    const value = input.value;
+    console.log(value) ;
+    return value ;
+}
+
+// machine id -> balance 
+
+function getBalance(){
+    const balanceElement = document.getElementById('balance');
+    const balance = balanceElement.innerText;
+    console.log("Current Balance",Number(balance))
+    return Number(balance);
+}
+
+// machine id -> set Balance 
+
+function setBalance(value){
+    const balanceElement = document.getElementById('balance');
+    balanceElement.innerText = value ;
+ }
